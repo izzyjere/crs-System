b4cc5b0e60e012423f393faf1cd89b11e5004c0c
izzyjere <wisdomjere5@gmail.com>
2022-09-26 07:16:21 +0200

    refactor

--- CRS.Client/Main.Designer.cs
+++ CRS.Client/Main.Designer.cs
@@ -40,21 +40,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(241, 34);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(181, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(471, 46);
+            this.label1.Size = new System.Drawing.Size(343, 37);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Criminal Records System";
+            this.label1.Text = "Crime Records System";
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Red;
             this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button3.Location = new System.Drawing.Point(409, 474);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(307, 385);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(223, 47);
+            this.button3.Size = new System.Drawing.Size(167, 38);
             this.button3.TabIndex = 5;
             this.button3.Text = "Exit Application";
             this.button3.UseVisualStyleBackColor = false;
@@ -65,10 +63,9 @@
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(308, 206);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(231, 167);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(228, 137);
+            this.button2.Size = new System.Drawing.Size(171, 111);
             this.button2.TabIndex = 3;
             this.button2.Text = "Cases";
             this.button2.UseVisualStyleBackColor = false;
@@ -79,10 +76,9 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(47, 206);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(35, 167);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(228, 137);
+            this.button1.Size = new System.Drawing.Size(171, 111);
             this.button1.TabIndex = 4;
             this.button1.Text = "Register Suspect";
             this.button1.UseVisualStyleBackColor = false;
@@ -93,10 +89,9 @@
             this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button4.Location = new System.Drawing.Point(557, 206);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Location = new System.Drawing.Point(418, 167);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(228, 137);
+            this.button4.Size = new System.Drawing.Size(171, 111);
             this.button4.TabIndex = 4;
             this.button4.Text = "View Records";
             this.button4.UseVisualStyleBackColor = false;
@@ -107,10 +102,9 @@
             this.button62.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.button62.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button62.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button62.Location = new System.Drawing.Point(811, 206);
-            this.button62.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button62.Location = new System.Drawing.Point(608, 167);
             this.button62.Name = "button62";
-            this.button62.Size = new System.Drawing.Size(228, 137);
+            this.button62.Size = new System.Drawing.Size(171, 111);
             this.button62.TabIndex = 3;
             this.button62.Text = "Judgements";
             this.button62.UseVisualStyleBackColor = false;
@@ -118,16 +112,15 @@
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button62);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main";
             this.Text = "Main";
             this.ResumeLayout(false);
